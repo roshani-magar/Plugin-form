bd7cc22f1303025e85226b3f73c1adfd358174c2
Roshani-magar <magarrosahni389@gmail.com>
2024-02-10 13:45:09 +0545

    index.php added

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/*
+ * Plugin Name:       Roshani Form
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Roshani Rana Magar
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+?>
